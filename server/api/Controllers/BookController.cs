dfafcf23887c13c4a9aafbb6f7ffdaff0a8bdcc7
Casper <casfro01@easv365.dk>
2025-09-15 17:23:32 +0200

    so far

---
+++ server/api/Controllers/BookController.cs
@@ -0,0 +1,33 @@
+﻿using dataaccess;
+using Microsoft.AspNetCore.Mvc;
+using services.Abstractions;
+
+namespace api.Controllers;
+
+[ApiController]
+public class BookController(IService<BaseBookRequest, BaseBookResponse> bookService) : ControllerBase
+{
+    [HttpGet(nameof(GetBooks))]
+    public List<Book> GetBooks()
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpGet(nameof(GetBook))]
+    public Book GetBook(string id)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpPost(nameof(CreateBook))]
+    public Book CreateBook([FromBody] CreateBookDto dto)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpPut(nameof(CreateBook))]
+    public Book UpdateBook([FromBody] UpdateBookDto dto)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ server/services/Abstractions/IService.cs
@@ -0,0 +1,18 @@
+﻿namespace services.Abstractions;
+
+// Rename ? -> reposity designpattern?
+/// <summary>
+/// Repository abstraction
+/// </summary>
+/// <typeparam name="TReq">Request type</typeparam>
+/// <typeparam name="TRes">Response type</typeparam>
+public interface IService<in TReq, TRes>
+{
+    public List<TRes> Get();
+    public TRes Get(string id);
+
+    public TRes Create(TReq request);
+    public TRes Update(TReq request);
+    public TRes Delete(TReq request);
+    public TRes Delete(string id);
+}
