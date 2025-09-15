9e7557f705c5fe3aa63c2c55657c02044aab8c2f
Casper <casfro01@easv365.dk>
2025-09-15 10:41:27 +0200

    auto generering

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,6 +1,49 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System.Text.Json.Serialization;
+using DefaultNamespace;
+using Microsoft.EntityFrameworkCore;
 
-app.MapGet("/", () => "Hello World!");
+namespace api;
+public class Program
+{
+    public static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSingleton<AppOptions>(provider =>
+        {
+            var configuration = provider.GetRequiredService<IConfiguration>();
+            var appOptions = new AppOptions();
+            configuration.GetSection(nameof(AppOptions)).Bind(appOptions);
+            return appOptions;
+        });
+        services.AddOpenApiDocument();
+        // add scoped services
+        services.AddProblemDetails();
 
-app.Run();
+        /* dbContext
+        services.AddDbContext<DbContext>((services, options) =>
+        {
+            options.UseNpgsql(services.GetService<AppOptions>().DbConnectionString);
+        });
+        */
+
+        services.AddControllers().AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        });
+    }
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        ConfigureServices(builder.Services);
+        
+        var app = builder.Build();
+        app.MapControllers();
+        app.UseOpenApi();
+        app.UseSwaggerUi();
+
+        // config færdig her
+        app.GenerateApiClientsFromOpenApi("/../client/src/LibAPI.ts").GetAwaiter().GetResult();
+        
+        app.Run();
+        
+    }
+}
