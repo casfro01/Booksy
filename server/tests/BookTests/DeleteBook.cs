ca210a0a93405176b921066d6b666f02ae52ff6e
Casper <casfro01@easv365.dk>
2025-09-23 10:28:03 +0200

    Tests til bog-opdatering og sletning

---
+++ server/tests/BookTests/DeleteBook.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using dataaccess;
+using services.Abstractions;
+using services.DTOs.Request;
+using services.DTOs.Response;
+
+namespace tests.BookTests;
+
+public class DeleteBook(MyDbContext ctx, IService<BaseBookResponse, CreateBookDto, UpdateBookDto> service)
+{
+    [Fact]
+    public async Task DeleteBookTest_ValidID()
+    {
+        var id = Guid.NewGuid().ToString();
+        var book = new Book
+        {
+            Id = id,
+            Title = "Test",
+            Pages = 11,
+            Authors = new List<Author>(),
+            Createdat = DateTime.UtcNow,
+        };
+        ctx.Books.Add(book);
+        await ctx.SaveChangesAsync(TestContext.Current.CancellationToken);
+        Assert.Equal(1, ctx.Books.Count());
+
+        var response = await service.Delete(id);
+        Assert.NotNull(response);
+        Assert.Equal(id, response.Id);
+        Assert.False(ctx.Books.Any());
+    }
+
+    [Fact]
+    public async Task DeleteBookTest_InvalidID()
+    {
+       await Assert.ThrowsAnyAsync<InvalidOperationException>(async () => await service.Delete(Guid.NewGuid().ToString()));
+    }
+}
---
+++ server/tests/BookTests/UpdateBook.cs
@@ -0,0 +1,52 @@
+﻿using dataaccess;
+using services.Abstractions;
+using services.DTOs.Request;
+using services.DTOs.Response;
+
+namespace tests.BookTests;
+
+public class UpdateBook(MyDbContext ctx, IService<BaseBookResponse, CreateBookDto, UpdateBookDto> service)
+{
+    [Fact]
+    public async Task UpdateBook_Success()
+    {
+        var id = Guid.NewGuid().ToString();
+        var book = new Book
+        {
+            Id = id,
+            Title = "Test",
+            Pages = 11,
+            Authors = new List<Author>(),
+            Createdat = DateTime.UtcNow,
+        };
+        ctx.Books.Add(book);
+        await ctx.SaveChangesAsync(TestContext.Current.CancellationToken);
+        Assert.Equal(1, ctx.Books.Count());
+
+        var updatedBook = new UpdateBookDto
+        {
+            Id = id,
+            Title = "new title",
+            Pages = 12,
+        };
+        await service.Update(updatedBook);
+        var actual = ctx.Books.First();
+        
+        Assert.Equal(updatedBook.Title, actual.Title);
+        Assert.Equal(updatedBook.Pages, actual.Pages);
+    }
+
+    [Fact]
+    public async Task UpdateBook_Fail()
+    {
+        var id = Guid.NewGuid().ToString();
+        var updatedBook = new UpdateBookDto
+        {
+            Id = id,
+            Title = "new title",
+            Pages = 12,
+        };
+        
+        await Assert.ThrowsAnyAsync<InvalidOperationException>(async () => await service.Update(updatedBook));
+    }
+}
